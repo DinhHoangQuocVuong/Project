58f9c730b0d10dd28d4d238dee5c1653168e31cb
DinhHoangQuocVuong <dhqvuong0210@gmail.com>
2024-04-17 21:05:08 +0700

    init project

---
+++ workspace/new-jdbc-04-2024/src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
@@ -0,0 +1,35 @@
+package com.laptrinhjavaweb.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.laptrinhjavaweb.dao.ICategoryDao;
+import com.laptrinhjavaweb.dao.impl.CategoryDao;
+
+@WebServlet(urlPatterns = { "/admin-home" })
+public class HomeController extends HttpServlet {
+
+	private static final long serialVersionUID = 2686801510274002166L;
+
+private ICategoryDao categoryDao;
+	
+	public HomeController() {
+		categoryDao = new CategoryDao()	;
+		}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("views/admin/home.jsp");
+		rd.forward(request, response);
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+}
---
+++ workspace/new-jdbc-04-2024/src/main/java/com/laptrinhjavaweb/service/impl/NewService.java
@@ -0,0 +1,8 @@
+package com.laptrinhjavaweb.service.impl;
+
+import com.laptrinhjavaweb.service.INewService;
+
+public class NewService implements INewService{
+
+	
+}
